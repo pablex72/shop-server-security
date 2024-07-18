dae2390cb9c21efdef0a59eb4ba47aadcd85b2fb
pablo <tonyp2hoy@gmail.com>
2024-07-18 18:31:35 -0400

    feat: filling bbdd with registers preload

--- src/main/java/com/ecommerce/shop/ShopApplication.java
+++ src/main/java/com/ecommerce/shop/ShopApplication.java
@@ -1,7 +1,11 @@
 package com.ecommerce.shop;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ShopApplication {
@@ -10,4 +14,14 @@ public class ShopApplication {
 		SpringApplication.run(ShopApplication.class, args);
 	}
 
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+//	@Bean
+//	public CommandLineRunner createPasswordCommand(){
+//		return args -> {
+//			System.out.println(passwordEncoder.encode("clave123"));
+//			System.out.println(passwordEncoder.encode("clave456"));
+//		};
+//	}
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+insert into users (username,password, name,role) values ('lmarquez', 'luis marquez', '$2a$10$Q5w4fS53IvnwFt4kkxKQqeYTssixOjmCdui5VrEMWfb84AmxKt.wi','CUSTOMER');
+insert into users (username,password, name,role) values ('mhernandez', 'mario hernandez', '$2a$10$KIAuTqEa1hPm.DYPIaBORuqf4Lyb2Ok521G20FpOiGs5GpeXiPofm','ADMINISTRATOR');
+
+INSERT INTO product (name,price) VALUES ('Smartphone', 500.00);
+INSERT INTO product (name, price) VALUES ('Auriculares', 50.00);
+INSERT INTO product (name, price) VALUES ('Tablet', 300.00);
+
+INSERT INTO product (name, price) VALUES ('Camiseta', 25.00);
+INSERT INTO product (name, price) VALUES ('Pantalones', 35.00);
+INSERT INTO product (name, price) VALUES ('Zapatos', 45.00);
+
+INSERT INTO product (name, price) VALUES ('Balon de futbol', 20.00);
+INSERT INTO product (name, price) VALUES ('Raqueta de tenis', 80.00);
+
+INSERT INTO product (name, price) VALUES ('Aspiradora', 120.00);
+INSERT INTO product (name, price) VALUES ('Licuadora', 48.00)
