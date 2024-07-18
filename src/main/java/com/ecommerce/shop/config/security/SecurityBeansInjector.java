fb22c509216cb3a50081f782e2bc42cd8e9db9ae
pablo <tonyp2hoy@gmail.com>
2024-07-18 13:27:52 -0400

    feat: authentication provider bean/dao/encoder

--- src/main/java/com/ecommerce/shop/config/security/SecurityBeansInjector.java
+++ src/main/java/com/ecommerce/shop/config/security/SecurityBeansInjector.java
@@ -3,7 +3,10 @@ package com.ecommerce.shop.config.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,6 +19,13 @@ public class SecurityBeansInjector {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager(); //ProviderManager implements AuthenticationManager
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(); //setear el dao acceso bd
+        provider.setUserDetailsService(null); //setaer el encoder para comparar contrase
+
     }
 
 }
