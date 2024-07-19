4c61916844600e1f89b00b92881efe1527ca6aa2
pablo <tonyp2hoy@gmail.com>
2024-07-19 18:46:54 -0400

    feat: authenticationsevice

--- src/main/java/com/ecommerce/shop/controller/AuthenticationController.java
+++ src/main/java/com/ecommerce/shop/controller/AuthenticationController.java
@@ -2,13 +2,18 @@ package com.ecommerce.shop.controller;
 
 import com.ecommerce.shop.dto.AuthenticationRequest;
 import com.ecommerce.shop.dto.AuthenticationResponse;
+import com.ecommerce.shop.service.AuthenticationService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> login(
---
+++ src/main/java/com/ecommerce/shop/service/AuthenticationService.java
@@ -0,0 +1,36 @@
+package com.ecommerce.shop.service;
+
+import com.ecommerce.shop.dto.AuthenticationRequest;
+import com.ecommerce.shop.dto.AuthenticationResponse;
+import com.ecommerce.shop.entity.User;
+import com.ecommerce.shop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public AuthenticationResponse login(AuthenticationRequest authenticationRequest) {
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                authenticationRequest.getUsername(), authenticationRequest.getPassword()
+        );
+        authenticationManager.authenticate(authenticationToken); //ya logueado se obtendra el user de bbdd
+
+        User user = userRepository.findByUsername(authenticationRequest.getUsername()).get();
+//                .orElseThrow(()-> new RuntimeException("user not found from Authentication Service"));
+        //generar el dto del token
+        String jwt =jwtService.generateToken(user,generateExtraClaims(user));
+        return new AuthenticationResponse(jwt);
+    }
+
+    private Map<String,Object> generateExtraClaims(User user) {
+    }
+}
